b968ab0bc8b16c7690f5e8d3c2c5d754499e88b7
Zachary Bishop <Zach@commanddatasolutions.com>
2026-03-10 10:42:48 -0400

    init

---
+++ src/main.py
@@ -0,0 +1,12 @@
+import torch
+import data
+
+print("Torch version:", torch.__version__)
+print("CUDA available:", torch.cuda.is_available())
+print("Device count:", torch.cuda.device_count())
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+dataset = data.load_dailydialog()
+
+print(dataset["train"][0])
